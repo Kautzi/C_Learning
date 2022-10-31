4e0e98fd309c0d033afb8a6d934721ddb68861ca
Kautzi <alexanderkautz.ak@gmail.com>
2022-10-31 15:46:04 +0100

    Update Main.c

--- Chapter06_Pointers/ContentChapter6/Main.c
+++ Chapter06_Pointers/ContentChapter6/Main.c
@@ -3,9 +3,15 @@
 int main()
 {
     int value = 12;
-
     printf("Der Wert von Value ist %d", value);
 
-    printf("\nDie Adresse von Value lautet %x", &value);
+    printf("\nDie Adresse von Value lautet %p", &value);
+
+    //Pointer Variablen anlegen
+    int *my_pointer = &value;
+    printf("\nDer Wert von Value ist %d", *my_pointer);
+    printf("\nDie Adresse von Value lautet %p", my_pointer);
+
+
     return 0;
 }
