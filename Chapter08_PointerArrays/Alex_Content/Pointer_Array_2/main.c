5ffe0677b5d14512e4fa074918c9f0231eb5a87a
Kautzi <alexanderkautz.ak@gmail.com>
2023-02-24 13:31:45 +0100

    NEXT Exercis Vid. 95

--- Chapter08_PointerArrays/Alex_Content/Pointer_Array_2/main.c
+++ Chapter08_PointerArrays/Alex_Content/Pointer_Array_2/main.c
@@ -9,23 +9,26 @@ for thatarray
 */
 
 
-#define LENGTH 4
+#define LENGTH_ROW 4
+#define LENGTH_COL 5
+#define INIT_VALUE 23
 
-//### Fnction Declaration ###
+//### Function Declaration ###
 int * create_array(unsigned int length, int value);
 int * freeArray(int * array);
+int ** create_matrix(uint32_t row_length,uint32_t coll_length,int value);
+int ** free_matrix(int **matrix, uint32_t length_row);
+void printArray(int * array, uint32_t length);
+void printMatrix(int **matrix,uint32_t num_row, uint32_t num_coll);
 //### END Declaration ###
 
 int main()
 {
-    int * arr_1 = create_array(LENGTH,12345);
+    int ** matrix = create_matrix(LENGTH_ROW,LENGTH_COL,INIT_VALUE);
 
-    for(unsigned int i = 0; i < LENGTH; i++){
+    printMatrix(matrix,LENGTH_ROW,LENGTH_COL);
 
-        printf("%d\n",arr_1[i]);
-    }
-
-    arr_1 = freeArray(arr_1);
+    matrix = free_matrix(matrix,LENGTH_ROW);
 
     return 0;
 }
@@ -56,4 +59,45 @@ int *freeArray(int *array)
     }
 
     return NULL;
+}
+
+int **create_matrix(uint32_t row_length, uint32_t coll_length, int value)
+{
+    int **matrix = (int**)malloc(row_length * sizeof(int*));
+
+    for(uint32_t i = 0; i < row_length; i++)
+    {
+        matrix[i] = create_array(coll_length,value);
+
+    }
+    return matrix;
+}
+
+int **free_matrix(int **matrix, uint32_t length_row)
+{
+
+    for(uint32_t i = 0; i < length_row;i ++ )
+    {
+
+            matrix[i]= freeArray(matrix[i]);
+
+    }
+    free(matrix);
+    return NULL;
+}
+
+void printArray(int *array, uint32_t length)
+{
+    for( uint32_t i = 0; i < length; i++)
+    {
+        printf("%d\n",array[i]);
+    }
+}
+
+void printMatrix(int **matrix, uint32_t num_row, uint32_t num_coll)
+{
+    for(uint32_t i = 0; i < num_row; i++){
+
+        printArray(matrix[i],num_coll);
+    }
 }
