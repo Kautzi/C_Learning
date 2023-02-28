063b1c0d72cda180af73bf4373f74cd2a4bfb098
Kautzi <alexanderkautz.ak@gmail.com>
2023-02-28 06:09:15 +0100

    NEXT 105

--- Chapter09_CharArrays/Alex_Cont/Eindimensional/main.c
+++ Chapter09_CharArrays/Alex_Cont/Eindimensional/main.c
@@ -1,39 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+//### Func Definition ###
+
+char ** create_Friendbook(uint32_t num_friends)
+{
+
+    char** friends = (char**)malloc(num_friends * sizeof(char*));
+
+    if(friends == NULL)
+    {
+        return NULL;
+    }
+
+    for(uint32_t i = 0; i < num_friends;i++)
+    {
+        friends[i]=(char*)malloc(20 * sizeof(char));
+    }
+
+    return friends;
+
+}
+
+char ** freeFriendbook(char** friends,uint32_t num_friends)
+{
+    for(uint32_t i = 0; i < num_friends; i++ )
+    {
+        free(friends[i]);
+    }
+    free(friends);
+    return NULL;
+}
+
+//### END Definition ###
 
 int main()
 {
+    uint32_t num_friends = 3;
 
-// Stack Arrays
-char array1[] = {'J','a','n','\0'};//Erste \0 muss bei dieser Variante angegeben werden aber variable String länge
+    char** friends = create_Friendbook(num_friends);
 
+    strcpy(friends[0],"Alexander Kautz");
+    strcpy(friends[1],"IRIS Kautz");
+    strcpy(friends[2],"Michael Schloter");
 
-char array2[4] = {'J','a','n','\0'};//Auch hier muss \0 angegeben werden aber Länge von string abhängig von [4]
+    for(uint32_t i = 0 ; i < num_friends; i++)
+    {
+        printf("%s\n",friends[i]);
+    }
 
-
-char array3[] = "Hallo Alex\n";// \0 wird automatisch mit angegeben und länge wird automatisch bestimmt
-
-char array4[32] ={'\0'}; //hier wird ein array mit 32 elementen initialisiert und jeder member wird mit \0 initialisiert
-
-
-
-//Heap Arrays
-
-char * array5 = (char*)malloc(32 * sizeof(char));
-
-array5[0]='A';
-array5[1]='L';
-array5[2]='E';
-array5[3]='X';
-array5[4]='\0';
-
-printf("%s",array5);
-
-
-
-printf("%s",array3);
-
-
-
-return 0;
+    friends = freeFriendbook(friends,num_friends);
+    return 0;
 }
