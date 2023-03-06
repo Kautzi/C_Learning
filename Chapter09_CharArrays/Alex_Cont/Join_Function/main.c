e85a4f3b4f792998b5ed17d114f6bdbe749405e9
Kautzi <alexanderkautz.ak@gmail.com>
2023-03-06 06:17:17 +0100

    NEXT 110

--- Chapter09_CharArrays/Alex_Cont/Join_Function/main.c
+++ Chapter09_CharArrays/Alex_Cont/Join_Function/main.c
@@ -19,6 +19,13 @@ char * join_strings(char* string_1, char* delimiter, char * string_2);
 
 int main()
 {
+    char* string_1 = "Mein Name ist Alex";
+    char* delimiter = "==>";
+    char* string_2 = "Kautz";
+
+    char* new_string =join_strings(string_1,delimiter,string_2);
+
+    printf("%s",new_string);
 
     return 0;
 }
@@ -27,6 +34,41 @@ int main()
 
 char *join_strings(char *string_1, char *delimiter, char *string_2)
 {
-    char * new_string = (char*)calloc(sizeof(char),strlen(string_1)+strlen(delimiter)+strlen(string_2));
+    char * new_string = (char*)calloc(strlen(string_1)+strlen(delimiter)+strlen(string_2),sizeof(char));
+    size_t reset =(strlen(string_1)+strlen(delimiter)+strlen(string_2));
+    if(string_1 == NULL || string_2 == NULL || delimiter == NULL)
+    {
+    return NULL;
+    }
+
+    //first iterate string_1 till the end '\0'
+    while(*string_1 != '\0')
+    {
+    *new_string = *string_1;
+    new_string++;
+    string_1 ++;
+    }
+
+    //now concatenate the delimitter to string_1
+    while(*delimiter != '\0')
+    {
+    *new_string = *delimiter;
+    new_string++;
+    delimiter ++;
+    }
+    //at last concatenate string_2 to new string
+    while(*string_2 != '\0')
+    {
+    *new_string = *string_2;
+    new_string++;
+    string_2 ++;
+    }
+
+    //to terminate new_string set the last character to'\0'
+    //*new_string='\0';
+    
+    new_string -=reset ;
+
+
     return new_string;
 }
