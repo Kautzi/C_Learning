2271517e65187992ab47dca179f05c44b4032af1
Kautzi <alexanderkautz.ak@gmail.com>
2023-03-29 05:57:46 +0200

    NEXT test createDynamicArray

--- Chapter14_DynamicArray/Alex_cont/Dynamic_Array.h
+++ Chapter14_DynamicArray/Alex_cont/Dynamic_Array.h
@@ -1,12 +1,33 @@
-#ifndef DYNAMIC_ARRAY.H
-#define DYNAMIC_ARRAY.H
+#ifndef DYNAMIC_ARRAY_H
+#define DYNAMIC_ARRAY_H
 
 //### DEFINES ###
 
+#define INIT_CAPACITY (unsigned int) (10U) //Initial Capacity far function call createDynamicArray
+#define GROWTH_FACTOR (unsigned int) (2U)
+#define SHRINK_FACTOR (unsigned int) (2U)
+
 typedef struct dynamic_array{
-    //...
-}dynamic_array;
+    float * data;
+    unsigned int length;
+    unsigned int capacity; // >= length
+}dynamic_array_t;
 
+//### Function Declaration ###
+dynamic_array_t * createDynamicArray();
 
+void * freeDynamicArray(dynamic_array_t *array );
+
+void expandDynamicArray(dynamic_array_t *array);
+
+void shrinkDynamicArray(dynamic_array_t *array);
+
+void pushValue(dynamic_array_t *array, const float value);
+
+float popValue(dynamic_array_t *array);
+
+void cleanDynamicArray(dynamic_array_t *array);
+
+void printDynamicArray(const dynamic_array_t *array);
 
 #endif /* DYNAMIC_ARRAY.H */
--- Chapter14_DynamicArray/Alex_cont/main.c
+++ Chapter14_DynamicArray/Alex_cont/main.c
@@ -22,4 +22,38 @@ int main()
 }
 
 //### FUNC DEFINES ###
-//...
+dynamic_array_t * createDynamicArray()
+{
+
+    //allocate memory for initial Array
+    dynamic_array_t *array = (dynamic_array_t*)malloc(sizeof(dynamic_array_t));
+    if(array == NULL)
+    {
+    return NULL;
+    }
+    array->length = 0U;
+    array->capacity=INIT_CAPACITY;
+    array->data = (float*)calloc(array->capacity,sizeof(float));
+
+    if(array->data == NULL)
+    {
+     free(array);
+     return NULL;
+    }
+
+    return array;
+}
+
+void * freeDynamicArray(dynamic_array_t *array );
+
+void expandDynamicArray(dynamic_array_t *array);
+
+void shrinkDynamicArray(dynamic_array_t *array);
+
+void pushValue(dynamic_array_t *array, const float value);
+
+float popValue(dynamic_array_t *array);
+
+void cleanDynamicArray(dynamic_array_t *array);
+
+void printDynamicArray(const dynamic_array_t *array);
