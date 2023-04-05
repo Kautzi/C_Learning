4c34c0becea82465540bc94e30625e600fd2d8f8
Kautzi <alexanderkautz.ak@gmail.com>
2023-04-05 07:33:52 +0200

    NEXT 153

---
+++ Chapter16_SimpleCSVReader/Alex_Cont/CSV Reader/main.c
@@ -0,0 +1,31 @@
+/*### Description: ###
+
+This should be a programm to red CSV Data (Comma Seperated Values) like a,5; b,7 ....
+The goal is to implement functions to read the values out of a file at execution time of main with int main(int argc, char * argv)
+open and read the Values of data.txt sort and write the values int a new file
+
+#############################################################################*/
+//### INCLUDES ###
+#include <stdio.h>  //printf ...
+#include <stdlib.h> //malloc ...
+#include <string.h> //strncat ...
+//### END INCLUDES ###
+
+//### DEFINES ###
+//...
+//### END DEFINES ###
+
+//### FUNC DECLARATION ###
+//...
+//### END DECLARATION ###
+
+//### MAIN ###
+
+int main(int argc, char **argv)
+{
+
+
+return 0;
+}
+
+//### FUNCTION DEFINITION ###
