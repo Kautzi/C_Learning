a9059c8163ff003cd62e0bb52eb734bb7e691a8d
Kautzi <alexanderkautz.ak@gmail.com>
2023-04-12 06:09:16 +0200

    NEXT dokumentation read_simple_csv()

--- Chapter16_SimpleCSVReader/Alex_Cont/CSV_Reader/CsvReader.c
+++ Chapter16_SimpleCSVReader/Alex_Cont/CSV_Reader/CsvReader.c
@@ -7,7 +7,8 @@
 #include "table.h"
 
 //### DEFINES ###
-#define NEW_LINE_CHARACTER_ASCII ('\n')
+#define NEW_LINE_CHARACTER_ASCII ('\n') //for line count to check how much lines present
+#define BUFFER_SIZE (size_t)(128)
 //### END DEFINE ###
 
 //### FUNCTION DEFINITIONS ###
@@ -36,38 +37,64 @@ size_t line_count(const char *const file_path)//FIRST CHECK ->> PASS
     return count;
 }
 
-void read_simple_csv(const char *const file_path,records_t * const records)
+void read_simple_csv(const char *const file_path,records_t * const records)//FIRST CHECK ->>PASS
 {
     FILE *const fp = fopen(file_path,"r");
     if(fp == NULL)
     {
         fprintf(stderr,"NOT able to open the file: %s!\n",strerror(errno));
-        return -1;
     }
 
+    const size_t num_lines = line_count(file_path);
+
+    val_pairs_t * const values = create_pairs(num_lines);
+
+    if(values == NULL)
+    {
+        fclose(fp);
+        return;
+    }
+
+    for(size_t i = 0; i < num_lines; i++)
+    {
+        char buffer[BUFFER_SIZE]={'\0'};
+        fgets(buffer,BUFFER_SIZE,fp);
+
+        sscanf(buffer,"%c,%d",&values[i].value_a,&values[i].value_b);
+
+    }
+    fclose(fp);
+
+    records->values=values;
+    records->num=num_lines;
 
 
 
 
 
 
-    fclose(file_path);
+    fclose(fp);
 }
 
-void write_simple_csv(const char *const file_path,const records_t * const records)
+void write_simple_csv(const char *const file_path,const records_t * const records)//FIRST CHECK->>PASS
 {
     FILE *const fp = fopen(file_path,"w");
     if(fp == NULL)
     {
-        printf(stderr,"NOT able to open the file: %s!\n",strerror(errno));
-        return -1;
+        fprintf(stderr,"NOT able to open the file: %s!\n",strerror(errno));
+
+    }
+
+     for(size_t i = 0; i < records->num; i++)
+    {
+        fprintf(fp,"%c,%d\n",records->values[i].value_a,records->values[i].value_b);
+
     }
 
 
 
 
 
-
-    fclose(file_path);
+    fclose(fp);
 }
 //### END FUNCTION DEFINITIONS ###
